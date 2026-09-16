b96267cb36a76a9e7e2013cb520c6c5e908384e5
janakader-ux <janakader@gmail.com>
2026-09-16 21:38:04 +0100

    Activate verified DPC GA4 stream behind analytics consent

--- public/js/analytics-config.js
+++ public/js/analytics-config.js
@@ -1,2 +1,2 @@
-// Set only to the verified GA4 web-stream measurement ID for directorpersonalcode.uk.
-window.DPC_GA4_ID = '';
+// Verified GA4 web stream for https://directorpersonalcode.uk; enhanced measurement disabled.
+window.DPC_GA4_ID = 'G-F8YKTG54MX';
